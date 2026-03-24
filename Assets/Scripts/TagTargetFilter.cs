045905cdcc5aa8709c93d33c93c2e2f4ca48784a
kadu-ymd <carlosepy@al.insper.edu.br>
2026-03-24 12:25:53 -0300

    feat: added new socket interactors/interactables and starting implementing logic for door unlocking

--- Assets/Scripts/TagTargetFilter.cs
+++ Assets/Scripts/TagTargetFilter.cs
@@ -1,19 +1,17 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Filtering;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
-public class BackpackFilter : XRBaseTargetFilter
+public class TagTargetFilter : XRBaseTargetFilter
 {
-    [SerializeField] private string tagPermitida = "BackpackTag";
-
-    // Removido o método Process(IXRInteractor, IXRInteractable) pois não existe na base para sobrescrever
+    [SerializeField] private string tagPermitida;
 
     public override void Process(IXRInteractor interactor, List<IXRInteractable> targets, List<IXRInteractable> results)
     {
         results.Clear();
+
         foreach (var interactable in targets)
         {
             if (interactable.transform.CompareTag(tagPermitida))
